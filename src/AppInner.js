58dab82c2a07b472ee958c39220d5b19a82e3459
LCGeon <corjs8449@naver.com>
2023-07-04 00:30:49 +0900

    Feat: Nav와 Banner 추가

---
+++ src/AppInner.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Nav from './components/Nav';
+import Banner from './components/Banner';
+function AppInner() {
+  return (
+    <div>
+      <Nav />
+      <Banner />
+    </div>
+  );
+}
+
+export default AppInner;
