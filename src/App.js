0afd66d2fb34d8c88e6d2a9ce925b03067e52aff
LCGeon <corjs8449@naver.com>
2023-07-04 00:29:59 +0900

    Style: AppInner 태그 추가

--- src/App.js
+++ src/App.js
@@ -1,5 +1,10 @@
 import React from 'react';
+import AppInner from './AppInner';
 
 export default function App() {
-  return <div>App</div>;
+  return (
+    <>
+      <AppInner />
+    </>
+  );
 }
