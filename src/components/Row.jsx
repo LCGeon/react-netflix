8d9efcb34aac45b938a5005d4d307c5fd94df123
LCGeon <corjs8449@naver.com>
2023-07-07 14:32:28 +0900

    Feat: 메인 페이지에서 영화의 장르별로 리스트를 보여주는 Row 컴포넌트 추가

--- src/components/Row.jsx
+++ src/components/Row.jsx
@@ -1,7 +1,63 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import axios from '../api/axios.js';
+import styled from 'styled-components';
+import MovieModal from './MovieModal.jsx';
 
-function Row() {
-  return <div>Row</div>;
+function Row({ title, id, fetchUrl, isLarge }) {
+  const [movies, setMovies] = useState([]);
+  const [modalOpen, setModalOpen] = useState(false);
+  const [movieSelected, setMovieSelected] = useState({});
+
+  useEffect(() => {
+    // MovieData 불러오기
+    fetchMovieData();
+  }, []);
+
+  const fetchMovieData = async () => {
+    // api request 받아오기
+    const request = await axios.get(fetchUrl);
+    setMovies(request.data.results);
+  };
+
+  const handleClick = (movie) => {
+    //영화 상세 정보
+    setModalOpen(true);
+    setMovieSelected(movie);
+  };
+  return (
+    <S.RowContainer>
+      <h2>{title}</h2>
+      <div>
+        <div>
+          <span>{'<'}</span>
+        </div>
+        <div id={id}>
+          {movies.map((movie) => (
+            <img
+              key={movie.id}
+              alt={movie.name}
+              src={`https://image.tmdb.org/t/p/original/${
+                isLarge ? movie.poster_path : movie.backdrop_path
+              }`}
+              onClick={() => handleClick(movie)}
+              style={{ width: '260px' }}
+            />
+          ))}
+        </div>
+        <div>
+          <span>{'<'}</span>
+        </div>
+      </div>
+      {modalOpen && (
+        <MovieModal {...movieSelected} setModalOpen={setModalOpen} />
+      )}
+    </S.RowContainer>
+  );
 }
-
+const S = {
+  RowContainer: styled.section`
+    margin-left: 20px;
+    color: black;
+  `,
+};
 export default Row;
