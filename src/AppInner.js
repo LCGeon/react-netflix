e3e133e49ce745673387c186fe1f35863dc75c3e
LCGeon <corjs8449@naver.com>
2023-07-07 14:28:55 +0900

    AppInner에 Row 컴포넌트와 Footer 컴포넌트추가

--- src/AppInner.js
+++ src/AppInner.js
@@ -1,11 +1,35 @@
 import React from 'react';
 import Nav from './components/Nav';
 import Banner from './components/Banner';
+import Row from './components/Row';
+import requests from './api/requests';
+import Footer from './components/Footer';
+
 function AppInner() {
   return (
     <div>
       <Nav />
       <Banner />
+
+      <Row
+        title='NETFLIX ORIGINALS'
+        id='NO'
+        fetchUrl={requests.fetchNetflixOriginals}
+        isLargeRow
+      />
+      <Row title='Trending Now' id='TN' fetchUrl={requests.fetchTrending} />
+      <Row title='Top Rated' id='TR' fetchUrl={requests.fetchTopRated} />
+      <Row
+        title='Action Movies'
+        id='AM'
+        fetchUrl={requests.fetchActionMovies}
+      />
+      <Row
+        title='Comedy Movies'
+        id='CM'
+        fetchUrl={requests.fetchComedyMovies}
+      />
+      <Footer />
     </div>
   );
 }
