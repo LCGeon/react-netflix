b4bd2ed2af5c37905ab3ff39cf44e015bfd5ba54
LCGeon <corjs8449@naver.com>
2023-07-15 22:45:47 +0900

    Feat: 영화를 검색하는 인풋 및 메인 로고 클릭시 메인페이지로 이동하기 추가

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,8 +1,12 @@
 import React, { useEffect, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 import styled from 'styled-components';
 
 function Nav() {
   const [show, setShow] = useState(false);
+  const [searchValue, setSearchValue] = useState('');
+  const navigate = useNavigate();
+
   useEffect(() => {
     // scrollY를 50으로 내리면 nav에 백그라운드 색깔 지정
     window.addEventListener('scroll', () => {
@@ -16,12 +20,26 @@ function Nav() {
       window.removeEventListener('scroll', () => {});
     };
   }, []);
+  const handleChange = (e) => {
+    setSearchValue(e.target.value);
+    navigate(`/search?q=${e.target.value}`);
+  };
+  const handleLogo = () => {
+    navigate('/');
+  };
+
   return (
     <S.nav show={show}>
       <S.navLogo
         alt='Netflix logo'
+        onClick={handleLogo}
         src='https://upload.wikimedia.org/wikipedia/commons/thumb/0/08/Netflix_2015_logo.svg/170px-Netflix_2015_logo.svg.png'
-        onClick={() => window.location.reload()}
+      />
+      <S.navInput
+        value={searchValue}
+        onChange={handleChange}
+        type='text'
+        placeholder='영화를 검색해주세요.'
       />
       <S.navAvata
         alt='User Logged'
@@ -58,6 +76,17 @@ const S = {
     left: 40px;
     width: 80px;
     object-fit: contain;
+    cursor: pointer;
+  `,
+  navInput: styled.input`
+    position: fixed;
+    left: 50%;
+    transform: translate(-50%, 0);
+    background-color: rgba(0, 0, 0, 0.582);
+    border-radius: 5px;
+    color: white;
+    padding: 5px;
+    border: none;
   `,
 };
 
