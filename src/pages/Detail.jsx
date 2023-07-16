47bd669ccb70ac89365129d0a9b28ea89f62252d
LCGeon <corjs8449@naver.com>
2023-07-16 19:39:51 +0900

    Design: css 변경

--- src/pages/Detail.jsx
+++ src/pages/Detail.jsx
@@ -1,10 +1,33 @@
 import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import axios from '../api/axios';
+import styled from 'styled-components';
+const S = {
+  DetailSection: styled.section``,
+  DetailImg: styled.img`
+    width: 100%;
+    height: 100%;
+  `,
+  DetailInfo: styled.div`
+    background-color: #757575;
+    position: absolute;
+    top: 200px;
+    left: 240px;
+    border-radius: 15px;
+    color: white;
+    opacity: 0.9;
+    max-width: 900px;
+  `,
+  InfoTitle: styled.h1`
+    font-size: 37px;
+  `,
+  InfoOverview: styled.div``,
+};
 
 export default function Detail() {
   const { movieId } = useParams();
   const [movie, setMovie] = useState({});
+  console.log(movie);
 
   useEffect(() => {
     async function fetchData() {
@@ -17,11 +40,15 @@ export default function Detail() {
   if (!movie) return <div>...loading</div>;
 
   return (
-    <section>
-      <img
+    <S.DetailSection>
+      <S.DetailImg
         src={`https://image.tmdb.org/t/p/original/${movie.backdrop_path}`}
         alt='poster'
       />
-    </section>
+      <S.DetailInfo>
+        <S.InfoTitle>{movie.title}</S.InfoTitle>
+        <S.InfoOverview>{movie.overview}</S.InfoOverview>
+      </S.DetailInfo>
+    </S.DetailSection>
   );
 }
--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react';
 import { useLocation, useNavigate } from 'react-router-dom';
 import { useDebounce } from '../hooks/useDebounce';
 import axios from '../api/axios';
+import styled from 'styled-components';
 
 function Search() {
   const navigate = useNavigate();
@@ -25,40 +26,63 @@ function Search() {
         `/search/multi?include_adult=false&query=${searchTerm}`
       );
       setSearchResults(request.data.results);
+      console.log(request);
     } catch (error) {
       console.log('error', error);
     }
   };
 
+  console.log(searchResults);
+
   const renderSearchResults = () => {
     return searchResults.length > 0 ? (
-      <section>
+      <S.SearchLayout>
         {searchResults.map((movie) => {
           if (movie.backdrop_path !== null && movie.media_type !== 'person') {
             const movieImageUrl =
               'https://image.tmdb.org/t/p/w500' + movie.backdrop_path;
             return (
-              <div key={movie.id}>
-                <div onClick={() => navigate(`/${movie.id}`)}>
-                  <img src={movieImageUrl} alt='movie' />
-                </div>
-              </div>
+              <S.SearchResult
+                key={movie.id}
+                onClick={() => navigate(`/${movie.id}`)}
+              >
+                <h3 style={{ color: 'white' }}>{movie.title}</h3>
+                <img src={movieImageUrl} alt='movie' />
+              </S.SearchResult>
             );
           }
         })}
-      </section>
+      </S.SearchLayout>
     ) : (
-      <section>
-        <div>
+      <S.SearchLayout>
+        <S.SearchResultNone>
           <p>
             찾고자하는 검색어"{debouncedSearchTerm}"에 맞는 영화가 없습니다.
           </p>
-        </div>
-      </section>
+        </S.SearchResultNone>
+      </S.SearchLayout>
     );
   };
 
   return renderSearchResults();
 }
+
+const S = {
+  SearchLayout: styled.section`
+    display: flex;
+    padding: 100px;
+    flex-wrap: wrap;
+  `,
+  SearchResult: styled.div`
+    padding: 20px;
+  `,
+  SearchResultNone: styled.div`
+    width: 100%;
+    color: white;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+  `,
+};
 
 export default Search;
